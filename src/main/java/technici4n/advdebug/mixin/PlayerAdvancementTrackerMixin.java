5a7ceee0271ea2f0b3f53b1bc6dd9bcbda5e3264
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2020-11-16 12:20:20 +0100

    Version 1.0.2

--- src/main/java/technici4n/advdebug/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/technici4n/advdebug/mixin/PlayerAdvancementTrackerMixin.java
@@ -3,25 +3,25 @@ package technici4n.advdebug.mixin;
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import technici4n.advdebug.AdvDebug;
 
 @Mixin(PlayerAdvancementTracker.class)
-public class PlayerAdvancementTrackerMixin {
-    @Inject(at = @At("HEAD"), method = "updateDisplay")
-    private void updateDisplayHeadInject(Advancement advancement, CallbackInfo ci) {
+public abstract class PlayerAdvancementTrackerMixin {
+    @Shadow
+    protected abstract void updateDisplay(Advancement advancement);
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/PlayerAdvancementTracker;updateDisplay(Lnet/minecraft/advancement/Advancement;)V"), method = "updateDisplay", require = 1)
+    private void updateDisplayRedirect(PlayerAdvancementTracker tracker, Advancement advancement) {
         AdvDebug.recursionDepth++;
         if (AdvDebug.recursionDepth > 50) {
             String advId = advancement.getId().toString();
             String parentId = advancement.getParent() == null ? "(no parent)" : advancement.getParent().getId().toString();
-            AdvDebug.LOGGER.info("updateDisplay called with advancement {}, its parent is {}", advId, parentId);
+            AdvDebug.LOGGER.info("updateDisplay called with advancement {}, its parent is {}, depth is {}", advId, parentId, AdvDebug.recursionDepth);
         }
-    }
-
-    @Inject(at = @At("RETURN"), method = "updateDisplay")
-    private void updateDisplayTailInject(Advancement advancement, CallbackInfo ci) {
+        updateDisplay(advancement);
         AdvDebug.recursionDepth--;
     }
 }
