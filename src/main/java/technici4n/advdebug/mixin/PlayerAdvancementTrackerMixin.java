c1cf4fa6693b8fa970388d7c9d02529b93710d1e
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2020-11-19 09:31:11 +0100

    Make it work in obfuscated environments

--- src/main/java/technici4n/advdebug/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/technici4n/advdebug/mixin/PlayerAdvancementTrackerMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.advancement.AdvancementProgress;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.*;
@@ -44,6 +45,7 @@ public abstract class PlayerAdvancementTrackerMixin {
         }
     }
 
+    @Overwrite
     protected void updateDisplay(Advancement advancement) {
         updateDisplayDfs(advancement, new ReferenceOpenHashSet<>());
     }
